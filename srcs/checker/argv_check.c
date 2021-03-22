f34594bbcc8c4cd52e2a1859111ded18a94faeda
seo2im <seo2im6492@gmail.com>
2021-03-22 11:09:04 +0900

    Duplicate Fix update
    DO
    1. argv duplicate error fix
    
    TODO
    1. more good algorithm

--- srcs/checker/argv_check.c
+++ srcs/checker/argv_check.c
@@ -6,16 +6,29 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 15:19:47 by seolim            #+#    #+#             */
-/*   Updated: 2021/03/21 15:36:27 by seolim           ###   ########.fr       */
+/*   Updated: 2021/03/22 11:07:17 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-int argv_check(int argc, char *argv[])
+static int	is_in(int set[], int num, int len)
 {
 	int	i;
+
+	i = -1;
+	while (++i < len)
+		if (num == set[i])
+			return (FALSE);
+	return (TRUE);
+}
+
+int argv_check(int argc, char *argv[])
+{
+	int	set[argc];
+	int	i;
 	int	j;
+	
 	if (argc == 0)
 		return (ft_write_n("Error"));
 	i = 0;
@@ -27,6 +40,10 @@ int argv_check(int argc, char *argv[])
 			if (!ft_isdigit(argv[i][j]))
 				return (ft_write_n("Error"));
 		}
+		if (!is_in(set, ft_atoi(argv[i]), i))
+			return (ft_write_n("Error"));
+		else
+			set[i] = ft_atoi(argv[i]);
 	}
 	return (0);
 }
