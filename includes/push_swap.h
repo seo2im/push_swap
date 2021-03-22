745745ee43a2cec3af0a151e637c3d649d6ce154
seo2im <seo2im6492@gmail.com>
2021-03-22 11:01:20 +0900

    Simple algorithm update(ERROR FIX)
    simple algorithm have error which 3 4 1 2, so fix it
    
    DO
    1. min < top(a), not correct, so fix it
    
    TODO
    1. more algorithm
    2. duplicated argv

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 16:13:24 by seolim            #+#    #+#             */
-/*   Updated: 2021/03/21 16:40:50 by seolim           ###   ########.fr       */
+/*   Updated: 2021/03/22 10:46:48 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define PUSH_SWAP_H
 # include "exec.h"
 
-void	simple(t_stack *a, t_stack *b);
-int		push_swap_main(int argc, char *argv[]);
+static int  g_count = 0;
+
+void	    simple(t_stack *a, t_stack *b);
+int		    push_swap_main(int argc, char *argv[]);
 
 #endif
--- srcs/push_swap/simple.c
+++ srcs/push_swap/simple.c
@@ -6,11 +6,23 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 16:12:58 by seolim            #+#    #+#             */
-/*   Updated: 2021/03/21 16:42:00 by seolim           ###   ########.fr       */
+/*   Updated: 2021/03/22 11:00:45 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	ft_command(char *code, t_stack *a, t_stack *b)
+{
+	g_count++;
+	ft_write_n(code);
+	/*
+	ft_write("a : ");
+	print_stack(a);
+	ft_write("b : ");
+	print_stack(b);
+	*/
+}
 
 void simple(t_stack *a, t_stack *b)
 {
@@ -31,13 +43,25 @@ void simple(t_stack *a, t_stack *b)
 			{
 				push_node(a, temp);
 				r(a);
+				ft_command("ra", a, b);
 			}
 			else
+			{
 				push_node(a, temp);
+				s(a);
+				ft_command("sa", a, b);
+				r(a);
+				ft_command("ra", a, b);
+			}
 			temp = NULL;
 		}
 		p(a, b);
+		ft_command("pb", a, b);
 	}
 	while (b->size != 0)
+	{
 		p(b, a);
+		ft_command("pa", a, b);
+	}
+	printf("count : %d\n", g_count);
 }
